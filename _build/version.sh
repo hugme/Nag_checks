b9ddb965a3042e9c08b9bed379e8b347be3c0218
Joern Ott <ottjoe@box-krn0-vbx-v01.sf-bk.de>
2022-04-27 14:08:51 +0200

    Add rpm build script and spec files

---
+++ _build/version.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# see https://github.com/joernott/icinga-plugin-check_iostats/tags
+APPS="icinga-plugin-check_linux_net icinga-plugin-check_linux_memory icinga-plugin-check_linux_route icinga-plugin-check_iscsi"
+VERSION="0.1.0"
+RELEASE="1"
+URL="https://github.com/joernott/Nag_checks/archive/refs/tags/v${VERSION}.tar.gz"
+ARCH="noarch"
